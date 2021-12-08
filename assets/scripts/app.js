203f2c187954f01f94f87a9c2d57561997c8e285
faha1999 <kafahad1999@gmail.com>
2021-12-09 00:21:33 +0600

    activated order now btn

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -72,12 +72,19 @@ class ShoppingCart extends Component {
     this.cartItems = updatedItems;
   }
 
+  orderProducts() {
+    console.log('Ordering...');
+    console.log(this.items);
+  }
+
   render() {
     const cartEl = this.crateRootElement('section', 'cart');
     cartEl.innerHTML = `
     <h2>Total: \$${0}</h2>
     <button>Order Now!</button>
     `;
+    const orderButton = cartEl.querySelector('button');
+    orderButton.addEventListener('click', () => this.orderProducts());
     this.totalOutput = cartEl.querySelector('h2');
   }
 }
